1e9961b1c4cf595938cfdde4bf46ec2d75a59156
Akanksha Agrawal <akanksha.agrawal@kalvium.community>
2022-12-22 12:31:50 +0530

    changes

--- src/components/GallaryBody.js
+++ src/components/GallaryBody.js
@@ -8,6 +8,7 @@ export default function GallaryBody(props){
         imageData.map(image=>{
           return(
             <div key={image.id} className="column">
+                <h1>Kalvium Gallary</h1>
               <img src={image.img} alt=""></img>
             </div>
           )
